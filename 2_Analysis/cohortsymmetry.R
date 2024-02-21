8f5b848672e236f09a08cfd61837738b5bbfb317
daniellenewby <danielleanne.newby@gmail.com>
2024-02-21 23:15:08 +0000

    update

--- 2_Analysis/cohortsymmetry.R
+++ 2_Analysis/cohortsymmetry.R
@@ -1,5 +1,3 @@
-print("Getting cohort sequence postive controls")
-info(logger, "Getting cohort sequence postive controls")
 ########################
 # positive controls
 ########################
@@ -159,55 +157,10 @@ cdm <- CohortSymmetry::getCohortSequence(cdm = cdm,
 upper_gi_ulcer_benchmarkers <- CohortSymmetry::getSequenceRatios(cdm = cdm,
                                                                        outcomeTable = "gi_benchmarkers") %>% 
   mutate(Analysis = "Test Drugs",         
-         Index_name = cdm[["test_drugs"]] %>%  distinct(cohort_name) %>% 
-           pull(),
-         Marker_name = cdm[["upper_gi_ulcer"]] %>%  distinct(cohort_name) %>% 
-           pull()) 
-
-# %>% 
-#   readr::write_csv(file = paste0(here::here(output.folder),"/", cdm_name(cdm) ,"_" , cdm[["ache_inhibitors_com"]] %>%  distinct(cohort_name) %>% 
-#                                    pull() , "_" , cdm[["beta_blockers_com"]] %>%  distinct(cohort_name) %>% 
-#                                    pull() ,
-#                                  
-#                                  ".csv")) 
-
-# cdm[["test_drugs"]] <- union_all(cdm[["beta_blockers"]] %>% dplyr::mutate(cohort_definition_id = 1, cohort_name = "Beta Blockers"), 
-#                                               cdm[["bladder_anticholinergics"]] %>% dplyr::mutate(cohort_definition_id = 2, cohort_name = "Bladder Anticholinergics")) %>% 
-#   dplyr::compute()
-
-
-# putting it into one table is too large therefore doing per table
-# beta blockers ###########################################################
-cdm[["beta_blockers_com"]] <- cdm[["beta_blockers"]] %>% 
-  dplyr::mutate(cohort_definition_id = 6, cohort_name = "Beta Blockers") %>% 
-  dplyr::compute()
-
-cdm <- CohortSymmetry::getCohortSequence(cdm = cdm,
-                                         name = "ache_inhibitors_com_beta_blockers",
-                                         dateRange = c(starting_date, ending_date),
-                                         indexTable = "ache_inhibitors_com",
-                                         markerTable = "beta_blockers_com",
-                                         daysPriorObservation = 365,
-                                         indexWashout = 365,
-                                         markerWashout = 365,
-                                         timeGap = 365)
-
-ache_inhibitors_com_beta_blockers <- CohortSymmetry::getSequenceRatios(cdm = cdm,
-                                                           outcomeTable = "ache_inhibitors_com_beta_blockers") %>% 
-  mutate(Analysis = "Test Drugs",         
-         Index_name = cdm[["ache_inhibitors_com"]] %>%  distinct(cohort_name) %>% 
-           pull(),
-         Marker_name = cdm[["beta_blockers_com"]] %>%  distinct(cohort_name) %>% 
-           pull()) %>% 
-           readr::write_csv(file = paste0(here::here(output.folder),"/", cdm_name(cdm) ,"_" , cdm[["ache_inhibitors_com"]] %>%  distinct(cohort_name) %>% 
-                                     pull() , "_" , cdm[["beta_blockers_com"]] %>%  distinct(cohort_name) %>% 
-                                     pull() ,
-    
-    ".csv")) 
-
-
-
-
-
-
-
+         Marker_name = "upper_gi_ulcer") %>% 
+  left_join(cdm[["test_drugs"]] %>% 
+             select("cohort_definition_id", "cohort_name") %>% 
+              distinct(),
+             by = c("index_id"="cohort_definition_id"),
+            copy = TRUE) %>% 
+  rename(Index_name = cohort_name)
