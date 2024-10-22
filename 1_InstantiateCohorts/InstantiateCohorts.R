ec3ff720343f84fa862fb3afa0309e1c532a8c21
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-10-22 11:12:42 +0100

    changing log files

--- 1_InstantiateCohorts/InstantiateCohorts.R
+++ 1_InstantiateCohorts/InstantiateCohorts.R
@@ -1,7 +1,5 @@
-cli::cli_alert_info("- Getting benchmarker definitions drug - drug")
-
 # positive controls -------
-cli::cli_alert_info("- Getting benchmarker definitions drug - drug positive controls")
+log("- Getting benchmarker definitions drug - drug positive controls")
 tic()
 cdm <- DrugUtilisation::generateIngredientCohortSet(
   cdm = cdm,
@@ -22,7 +20,7 @@ toc()
 cli::cli_alert_success("- Got benchmarker definitions drug - drug positive controls")
 
 # negative controls -------
-cli::cli_alert_info("- Getting benchmarker definitions drug - drug negative controls")
+log("- Getting benchmarker definitions drug - drug negative controls")
 
 cdm <- generateIngredientCohortSet(
   cdm = cdm,
@@ -33,7 +31,7 @@ cdm <- generateIngredientCohortSet(
 
 cli::cli_alert_success("- Got benchmarker definitions drug - drug negative controls")
 
-cli::cli_alert_info("- Getting benchmarker definitions conditions")
+log("- Getting benchmarker definitions conditions")
 
 bm_conditions_csv <- read_csv(
   here::here("3_Markers", "conditions_ade.csv"),
@@ -65,81 +63,7 @@ rm(bm_conditions_csv)
 cli::cli_alert_success("- Got benchmarker definitions drug-conditions (conditions)")
 
 # get drug list for benchmarkers
-cli::cli_alert_info("- Getting benchmarker definitions drug-conditions (drugs)")
-# data(euadrReferenceSet)
-# data(omopReferenceSet)
-# 
-# euadrReferenceSet <- euadrReferenceSet |>
-#   mutate(exposureName = tolower(as.character(exposureName))) |>
-#   mutate(exposureName = ifelse(exposureName == "regular insulin, human", "insulin, regular, human", exposureName),
-#          exposureName = ifelse(exposureName == "thyroxine", "levothyroxine", exposureName)) |>
-#   mutate("referenceSet" = "EU ADR" )
-# 
-# drugs <- euadrReferenceSet |>
-#   distinct(exposureName) |>
-#   pull(exposureName)
-# 
-# omopReferenceSet <- omopReferenceSet |>
-#   mutate(exposureName = tolower(as.character(exposureName))) |>
-#   mutate(exposureName = ifelse(exposureName == "estrogens, conjugated (usp)", "estrogens, conjugated (USP)", exposureName)) |>
-#   mutate("referenceSet" = "OMOP Reference Set" )
-# 
-# combined_adr <- bind_rows(
-#   euadrReferenceSet,
-#   omopReferenceSet
-# ) |>
-#   dplyr::mutate(outcomeName = stringr::str_replace_all(outcomeName, "[:digit:]", "")) |>
-#   dplyr::mutate(outcomeName = stringr::str_replace_all(outcomeName, "#$", "")) |>
-#   dplyr::mutate(outcomeName = stringr::str_trim(outcomeName)) |>
-#   dplyr::mutate(outcomeName = stringr::str_replace_all(outcomeName, "^OMOP ", "")) |>
-#   dplyr::mutate(outcomeName = tolower(as.character(outcomeName))) |>
-#   dplyr::mutate(outcomeName = stringr::str_replace(outcomeName, "-", " ")) |>
-#   dplyr::mutate(outcomeName = stringr::str_replace_all(outcomeName, " ", "_")) |>
-#   dplyr::mutate(outcomeName = case_when(
-#     (outcomeName == "hoi_upper_gi") ~ "upper_gi_ulcer",
-#     (outcomeName == "leukopenia_including_neutropenia_and_agranulocytosis") ~ "neutropenia",
-#     T ~ outcomeName
-#   ))
-
-# drugs1 <- omopReferenceSet |>
-# distinct(exposureName) |>
-#   pull(exposureName)
-# 
-# drugs <- c(drugs, drugs1) |>
-#   unique()
-# 
-# readr::write_csv(combined_adr,
-#                  paste0(here::here(output_folder),"/", cdm_name(cdm), "_reference_standards.csv"))
-# 
-#   # create a loop that instantiates each drug cohort
-#   cli_progress_bar("Instanstiating cohorts", total = length(drugs))
-# 
-#   for (i in 1:length(drugs)) {
-# 
-#     Sys.sleep(10/100)
-# 
-#     cdm <- generateIngredientCohortSet(
-#       cdm = cdm,
-#       name = drugs[i],
-#       ingredient = drugs[i],
-#       durationRange = c(1, Inf),
-#       imputeDuration = "none",
-#       gapEra = 30,
-#       priorUseWashout = 0,
-#       priorObservation = 0,
-#       cohortDateRange = as.Date(c(starting_date, ending_date)),
-#       limit = "all",
-#       doseForm = NULL,
-#       ingredientRange = c(1, Inf)
-#     )
-# 
-#     cli_progress_update()
-# 
-#     success_message <- paste("- Benchmarker Cohorts generated for CohortSymmetry for", drugs[i])
-# 
-#     # Print the success message
-#     cli::cli_alert_success(success_message)
-#   }
+log("- Getting benchmarker definitions drug-conditions (drugs)")
 
 oxfordRef <- read_excel(here::here("3_Markers", "oxford_reference.xlsx")) |>
   dplyr::mutate(
@@ -248,8 +172,6 @@ for (i in 1:length(atc_events)) {
     cdm[[cohort]] <- cdm[[cohort]] |> 
       CohortConstructor::unionCohorts(cohortName = cohort)
   }
-
-cli::cli_alert_success("- Got benchmarker definitions drug-conditions (drugs)")
 
 cli::cli_alert_success("- Got benchmarker definitions drug - conditions")
 
